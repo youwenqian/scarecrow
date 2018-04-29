8a00ed6515ff73cda5560550570973ad8a9e30a1
youwenqian <yiliusan314@163.com.cn>
2018-04-29 17:32:57 +0800

    添加前端部分

---
+++ modules/shoes-dao/src/main/java/com/shoes/scarecrow/persistence/domain/TypeCondition.java
@@ -0,0 +1,23 @@
+package com.shoes.scarecrow.persistence.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author li
+ * @description
+ * @time 2018/4/28 14:16
+ */
+@Data
+public class TypeCondition extends BaseCondition implements Serializable{
+    private static final long serialVersionUID = -6036660988814972212L;
+    private Integer id;
+    private String typeName;
+
+    private Integer page;
+
+    private Integer pageSize;
+
+    private Integer startRow;
+}
