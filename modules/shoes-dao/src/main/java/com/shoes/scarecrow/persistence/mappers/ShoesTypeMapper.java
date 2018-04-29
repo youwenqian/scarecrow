604a685fd442b093f6d9be6042945bc924d80e03
youwenqian <yiliusan314@163.com.cn>
2018-04-29 19:06:02 +0800

    添加前端部分

---
+++ modules/shoes-dao/src/main/java/com/shoes/scarecrow/persistence/mappers/ShoesTypeMapper.java
@@ -0,0 +1,25 @@
+package com.shoes.scarecrow.persistence.mappers;
+
+import com.shoes.scarecrow.persistence.domain.ShoesType;
+import com.shoes.scarecrow.persistence.domain.TypeCondition;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("shoesTypeMapper")
+public interface ShoesTypeMapper {
+    int insert(ShoesType record);
+
+    List<ShoesType> queryByCondition(TypeCondition condition);
+
+    int queryCountByCondition(TypeCondition condition);
+
+    ShoesType queryById(int id);
+
+    ShoesType queryByName(@Param("name") String name);
+
+    int update(ShoesType record);
+
+    int delById(int id);
+}
